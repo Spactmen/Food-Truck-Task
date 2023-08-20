e0f30d66be90cf4d40a19626bb00c7a5f83b5a5c
siddhi vigneshwar <siddhivigneshwar@gmail.com>
2023-08-20 10:55:59 +0530

    second commit

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -12,7 +12,7 @@ const AboutUs = () => {
         </div>
         <div className={styles.about_data}>
           <h2>About US</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. t has survived not only five centuries.</p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. t has survived not only five centuries.</p>
           <button>Read More</button>
         </div>
       </div>
